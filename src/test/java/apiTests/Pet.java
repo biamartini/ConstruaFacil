9e705d890b5b12da4e145775d94c8cc656603bd2
biamartini <bianca.martini.evs@gmail.com>
2022-03-10 16:43:29 -0300

    mais exerccicios

--- src/test/java/apiTests/Pet.java
+++ src/test/java/apiTests/Pet.java
@@ -1,13 +1,21 @@
 package apiTests;
 
+import org.hamcrest.Matcher;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.stringContainsInOrder;
 
 public class Pet {
+
+    String uri = "https://petstore.swagger.io/v2/pet";
+    int petId = 1030;
     //Gerki
     //Given - Dado
     //.When - Quando
@@ -16,22 +24,74 @@ public class Pet {
     // funcoes de apoio
 
 
-      public String lerjson(String caminhoJson){
+      public String lerJson(String caminhoJson) throws IOException {
 
           return new String(Files.readAllBytes(Paths.get(caminhoJson)));
     }
 
-    @Test
-    public void incluirPet(){//create - post
+    @Test (priority = 0)
+    public void incluirPet() throws IOException { //create - post
+        String jsonBody = lerJson("src/test/resources/data/pet.json");
+        given()
+                .contentType("application/json")
+                .log().all()
+                .body(jsonBody)
+                .when()
+                .post(uri)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("id", is(petId))
+                .body("name", is("mumi"))
+                .body("category.name", is("cat"))
+                .body("tags.name[0]", stringContainsInOrder("vermifugado"))
 
-
+        ;
+    }
+@Test(priority = 1,dependsOnMethods = {"incluirPet"})
+    public void consultarPet() {
+    //String petId = petId;
     given()
             .contentType("application/json")
             .log().all()
         .when()
-            .post("https://petstore.swagger.io/v2/pet")
+            .get(uri + "/" + petId)
         .then()
             .log().all()
+            .statusCode(200)
+            .body("name",is("mumi"))
+            .body("status",is("available"))
+    ;
+}
+@Test(priority = 1, dependsOnMethods = {"consultarPet"})
+    public void alterarPet() throws IOException {
+    String jsonBody = lerJson("src/test/resources/data/newpet.json");
+    given()
+
+        .contentType("application/json")
+        .log().all()
+        .body(jsonBody)
+    .when()
+        .put(uri)
+    .then()
+        .statusCode(200)
+        .log().all()
+        .body("status",is("sold"))
         ;
+}
+@Test
+        public void excluirPet(){
+    Object integer;
+    given()
+            .contentType("application/json")
+            .log().all()
+        .when()
+            .delete(uri + "/" + petId)
+        .then()
+            .log().all()
+            .statusCode(200)
+            .body("code",is(200))
+            .body("type",is("unknown"))
+            .body("message",is(Integer.toString(petId)));
 }
 }
